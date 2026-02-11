2d41255eed752192a11eaaad60fb89bc6bb780fe
RJ Mattson <rjmattson@crimson.ua.edu>
2026-02-11 01:12:30 -0600

    Should be final version

--- Spring2026-Project1-rjmattson/wwwroot/js/site.js
+++ Spring2026-Project1-rjmattson/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let count = 0;
+const button = document.getElementById("theButton");
+const display = document.getElementById("Likes");
+
+button.addEventListener('click', () => {
+    count++;
+    display.innerText = count + " Likes";
+});
